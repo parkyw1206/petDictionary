c09231caccbf70e4ede2bcac4b4d642965ab92f5
Youngwoo Park <36143152+parkyw1206@users.noreply.github.com>
2022-04-07 23:32:04 +0900

    firebase연동 및 app화면으로 옮김

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { } from 'firebase/firestore';
+
+function App() {
+  return (
+    <div className="App">
+      하이
+    </div>
+  );
+}
+
+export default App;
